3c36b082e023a294abc464f2376ad4fe6e069c87
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2023-12-06 23:18:44 -0300

    Cambio numero temporada, sigo agregando javascript

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 import { printError, getJson } from './aux.js';
 
+var categorias = ["equipamiento","minerales","curacion","nether","misc","inventario"]
+
 async function loadStats(temp, ep, jugador, equipo) {
     if (equipo == null) {
-        var jugador_data = await getJson("../data/"+temp+"/equipos.json");
-        var jugador_actual = jugador_data[jugador];
-        var ep_data = await getJson("../data/"+temp+"/"+ep+".json");
-        var ep_actual = ep_data[jugador];
-        updateStats(jugador_actual, ep_actual, 1);
+        var info_jugadores = await getJson("../data/"+temp+"/equipos.json");
+        var info_jugador = info_jugadores[jugador];
+        var stats_jugadores = await getJson("../data/"+temp+"/"+ep+".json");
+        var stats_jugador = stats_jugadores[jugador];
+        updateStats(info_jugador, stats_jugador, 1);
     } else {
         var equipo_data = await getJson("../data/"+temp+"/equipos.json");
         //equipo_data[equipo];
     }
 }
 
-function updateStats(jugador_actual, ep_actual, numJugador) {
-
+function updateStats(info_jugador, stats_jugador, numJugador) {
+    let p = document.getElementById("displayname_"+numJugador); // Encuentra el elemento "p" en el sitio
+    p.innerHTML = info_jugador["nombre"]
+    categorias.forEach(categoria => {
+        let attr = categoria+"_"+numJugador;
+        let p = document.getElementById(attr); // Encuentra el elemento "p" en el sitio
+        console.log("JUGADOR: "+info_jugador["nombre"]+"\nCategoria: "+categoria+"\nStats: "p);
+        //p.innerHTML = createContent(stats_jugador[attr]);
+    });
+    p.onclick = function(){
+        selectMap();
+        selectTexturePack();
+        _hideStartMenu();
+        document.getElementById("startMenu").style.display = "none";
+        document.getElementById("debugHover").style.display = "flex";
+    }
 }
