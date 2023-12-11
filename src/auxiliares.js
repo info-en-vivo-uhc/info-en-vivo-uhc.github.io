4a22bf8f74f82c9487b45711e5bf9642e764d982
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2023-12-11 17:58:08 -0300

    Equipamiento funcionando

--- src/auxiliares.js
+++ src/auxiliares.js
@@ -1,4 +1,4 @@
-export { printError, getJson }
+export { printError, getJson, IconPaths, Traductor }
 
 function printError(error, path, explicit) {
     console.log(`[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}, JSON Path: ${path}. This error probably occurs because JSON file has a syntax error or does not exist`);
@@ -16,4 +16,65 @@ async function getJson(path){
         }
     }
     return data;
+}
+
+class IconPaths {
+    //static hearts = "../minecraft-assets/assets/minecraft/textures/gui/sprites/hud/heart";    
+    static corazones = "../img/assets/";
+    static equipamiento = "../minecraft-assets/assets/minecraft/textures/item/";
+    static material = this.equipamiento;  
+    static minerales = this.equipamiento;  
+    static curacion = this.equipamiento;  
+}
+
+class Traductor {
+    static material = 
+    {
+        "hierro": "iron",
+        "oro": "golden",
+        "cuero": "leather",
+        "piedra": "stone",
+        "madera": "wooden",
+        "netherite": "netherite",
+        "cota de malla": "chainmail",
+        "diamante": "diamond"
+    };
+    static equipamiento = 
+    {
+        "pies": "boots",
+        "cuerpo":"chestplate",
+        "cabeza": "helmet",
+        "piernas": "leggings",
+        "hacha": "axe",
+        "hoz": "hoe",
+        "pico": "pickaxe",
+        "pala": "shovel",
+        "espada": "sword",
+        "arco": "bow",
+        "ballesta": "crossbow_arrow"
+    }
+    static minerales = this.material;
+    static curacion = 
+    {
+        "manzana": "apple",
+        "manzana_oro": "golden_apple",
+        "melon_oro": "glistering_melon_slice"
+    };
+    static nether_pociones =
+    {
+        "blazerod": "blaze_rod",
+        "ojoarana": "spider_eye"
+    };
+    static miscelaneo = 
+    {
+        "escudo": "shield",
+        "escudo_path": IconPaths.equipamiento
+    };
+    static traducirEquipamiento(tipo, valores) {
+        let material = ""
+        if (valores["material"] != null) {
+            material = Traductor.material[valores["material"]] + "_"
+        }
+        return material + Traductor.equipamiento[tipo] + ".png";
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { printError, getJson } from './auxiliares.js';
+import { printError, getJson, IconPaths, Traductor } from './auxiliares.js';
 
 var categorias = ["equipamiento","minerales","curacion","nether_pociones","miscelaneo","inventario"]
 
@@ -24,47 +24,78 @@ async function loadStats(temp, ep, jugador, equipo) {
 function updateStats(info_jugador, stats_jugador, numJugador) {
     let nameHTML = document.getElementById("displayname_"+numJugador); // Encuentra el elemento "p" en el sitio
     nameHTML.innerHTML = info_jugador["nombre"]
-    let vidaHTML = document.getElementById("vida_"+numJugador); // Encuentra el elemento "p" en el sitio
-    vidaHTML.appendChild(crearCorazones(stats_jugador["vida"]));
+    updateVida(numJugador, stats_jugador["vida"]);
     categorias.forEach(categoria => {
         let attr = categoria+"_"+numJugador;
         let categoryHTML = document.getElementById(attr); // Encuentra el elemento "p" en el sitio
-        console.log("JUGADOR: "+info_jugador["nombre"]+"\nCategoria: "+categoria+"\nStats: "+JSON.stringify(stats_jugador[categoria]));
-        categoryHTML.innerHTML = crearContenido(categoria, categoryHTML, stats_jugador[categoria]);
+        //console.log("JUGADOR: "+info_jugador["nombre"]+"\nCategoria: "+categoria+"\nStats: "+JSON.stringify(stats_jugador[categoria]));
+        crearLuegoAgregarContenido(categoria, categoryHTML, stats_jugador[categoria]);
     });
 }
 
-function crearCorazones(num) {
-    var elem = document.createElement("div");
+async function updateVida(numJugador, numVida) {
+    let vidaHTML = document.getElementById("vida_"+numJugador); // Encuentra el elemento "p" en el sitio
+    vidaHTML.appendChild(barraCorazones(numVida));
+}
+
+function barraCorazones(num) {
+    let elem = document.createElement("div");
     for (let i = 0; i < Math.floor(num); i++) {
-        let cora = document.createElement("img");
-        cora.setAttribute("src", "../img/assets/hardcore_full.png");
-        cora.setAttribute("height", "20px");
-        cora.setAttribute("width", "20px");
-        cora.setAttribute("alt", "Hardcore full heart");
-        elem.appendChild(cora);
+        elem.appendChild(crearImgElem(IconPaths.corazones+"hardcore_full.png"), "20px", "20px", "Hardcore full heart");
     }
     if (!Number.isInteger(num)) {
-        let medio_cora = document.createElement("img");
-        medio_cora.setAttribute("src", "../img/assets/hardcore_half.png");
-        medio_cora.setAttribute("height", "20px");
-        medio_cora.setAttribute("width", "20px");
-        medio_cora.setAttribute("alt", "Hardcore half heart");
-        elem.appendChild(medio_cora);
+        elem.appendChild(crearImgElem(IconPaths.corazones+"hardcore_half.png"), "20px", "20px", "Hardcore half heart");
     }
     // Chequeo si falta agregar contenedores de corazón vacío; redondeo para arriba porque el medio corazon cuenta como un contenedor completo.
     while (Math.round(num) < 10) {
-        let cora_vacio = document.createElement("img");
-        cora_vacio.setAttribute("src", "../img/assets/heart_container.png");
-        cora_vacio.setAttribute("height", "20px");
-        cora_vacio.setAttribute("width", "20px");
-        cora_vacio.setAttribute("alt", "Empty heart container");
-        elem.appendChild(cora_vacio);
+        elem.appendChild(crearImgElem(IconPaths.corazones+"heart_container.png"), "20px", "20px", "Empty heart container");
         num++;
     }
     return elem;
 }
 
-function crearContenido(categoria, categoryHTML, stats) {
-    return 1;
+function crearImgElem(path, height=null, width=null, alt=null) {
+    /*
+    let imgHTML = document.createElement("canvas");
+    let ctx = imgHTML.getContext("2d");
+    ctx.drawImage(path, 0, 0, width, height);
+    imgHTML.setAttribute("alt", alt);
+    */
+    let imgHTML = document.createElement("img");
+    imgHTML.setAttribute("src", path);
+    imgHTML.setAttribute("height", height);
+    imgHTML.setAttribute("width", width);
+    imgHTML.setAttribute("alt", alt);
+    return imgHTML;
+}
+
+async function crearLuegoAgregarContenido(categoria, categoryHTML, stats_jugador_en_categoria) {
+    let elem = document.createElement("div");
+    switch (categoria) {
+        case "equipamiento":
+            for (let k in stats_jugador_en_categoria) {
+                let nombreImg = Traductor.traducirEquipamiento(k, stats_jugador_en_categoria[k]);
+                let elemImg = crearImgElem(IconPaths.equipamiento+nombreImg, 32, 32, k + " " + stats_jugador_en_categoria[k]["material"]);
+                elem.appendChild(elemImg);
+            }
+            break;
+        case "minerales":
+            elem.innerText = 1;
+            break;
+        case "curacion":
+            elem.innerText = 2;
+            break;
+        case "nether_pociones":
+            elem.innerText = 3;
+            break;
+        case "miscelaneo":
+            elem.innerText = 4;
+            break;
+        case "inventario":
+            elem.innerText = 5;
+            break;
+        default:
+            elem.innerText = -1;
+      }
+      categoryHTML.appendChild(elem); 
 }
